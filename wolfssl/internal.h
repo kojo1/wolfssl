c39b4f0e9b398ae7865e7fb46ce7c66eacfbfd8c
Jacob Barthelmeh <jacob@wolfssl.com>
2017-03-31 15:39:21 -0600

    evp key flags and add WOLFSSL_BUF_MEM struct to WOLFSSL_BIO

--- wolfssl/internal.h
+++ wolfssl/internal.h
@@ -1252,6 +1252,7 @@ struct WOLFSSL_BIO_METHOD {
 
 /* wolfSSL BIO type */
 struct WOLFSSL_BIO {
+    WOLFSSL_BUF_MEM* mem_buf;
     WOLFSSL*     ssl;           /* possible associated ssl */
 #ifndef NO_FILESYSTEM
     XFILE        file;
