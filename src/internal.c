1e47145df6d29b3013c3ac3aade0c6667b5ae631
Marco Oliverio <marco@wolfssl.com>
2022-04-04 19:28:51 +0200

    internal: return from wolfSSL_Peek() with sz 0 if we don't have data
    This way we can use wolfSSL_Peek() invoked with sz == 0 to process pending
    records and, if none of this records is an application data record, we will not
    block.

--- src/internal.c
+++ src/internal.c
@@ -20498,6 +20498,15 @@ startScr:
                 }
             }
         #endif
+
+#ifdef WOLFSSL_DTLS13
+        if (ssl->options.dtls && IsAtLeastTLSv1_3(ssl->version) && peek &&
+            sz == 0 && ssl->buffers.inputBuffer.idx
+                - ssl->buffers.inputBuffer.length  == 0) {
+            return 0;
+        }
+#endif /* WOLFSSL_DTLS13 */
+
 #ifndef WOLFSSL_TLS13_NO_PEEK_HANDSHAKE_DONE
     #ifdef WOLFSSL_TLS13
         if (IsAtLeastTLSv1_3(ssl->version) && ssl->options.handShakeDone &&
